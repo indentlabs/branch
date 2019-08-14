12d21a199a4d973f46b4e595489cb1bf2f7f2d85
Andrew Brown <drusepth@gmail.com>
2019-08-14 15:03:06 -0500

    polishing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'static/index'
   get 'actions/create'
 
   resources :chunks
   resources :actions  
+
+  devise_for :users
   resources :users do
     get '/stories', to: 'users#stories', on: :member
   end
